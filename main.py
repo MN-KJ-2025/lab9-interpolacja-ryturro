c79d31c7378c7d7f4c69d1e559f24c7612653cc9
Artur Folwaczny <afolwaczny9@gmail.com>
2025-12-01 00:33:46 +0100

    gotowe

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import numpy as np
 
 
 def chebyshev_nodes(n: int = 10) -> np.ndarray | None:
+    nodes_array=[]
+    if type(n)==int:
+        for k in range(0,n):
+            nodes_array.append(np.cos((k*np.pi)/(n-1)))
+        return np.array(nodes_array)
+    else:
+        return None
     """Funkcja generująca wektor węzłów Czebyszewa drugiego rodzaju (n,) 
     i sortująca wynik od najmniejszego do największego węzła.
 
@@ -18,10 +25,26 @@ def chebyshev_nodes(n: int = 10) -> np.ndarray | None:
         (np.ndarray): Wektor węzłów Czebyszewa (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
 
 
 def bar_cheb_weights(n: int = 10) -> np.ndarray | None:
+    if type(n)==int:
+        weights_array=[]
+        for j in range(0,n):
+            if j==0 or j==n-1:
+                w=1/2
+            else:
+                w=1
+            if j==0 or j%2==0:
+                weights_array.append(w)
+            else:
+                weights_array.append(-w)
+        return np.array(weights_array)
+    else:
+        return None
+
+
+
     """Funkcja tworząca wektor wag dla węzłów Czebyszewa wymiaru (n,).
 
     Args:
@@ -37,6 +60,14 @@ def bar_cheb_weights(n: int = 10) -> np.ndarray | None:
 def barycentric_inte(
     xi: np.ndarray, yi: np.ndarray, wi: np.ndarray, x: np.ndarray
 ) -> np.ndarray | None:
+    Y=[]
+    for x in np.nditer(x):
+        if x in xi:
+            Y.append(yi[np.where(xi==x)[0][0]])
+        else:
+            L=wi/(x-xi)
+            Y.append(yi@L/sum(L)) 
+    return np.array(Y)
     """Funkcja przeprowadza interpolację metodą barycentryczną dla zadanych 
     węzłów xi i wartości funkcji interpolowanej yi używając wag wi. Zwraca 
     wyliczone wartości funkcji interpolującej dla argumentów x w postaci 
@@ -58,6 +89,11 @@ def barycentric_inte(
 def L_inf(
     xr: int | float | list | np.ndarray, x: int | float | list | np.ndarray
 ) -> float | None:
+    if type(xr)==int or type(xr)==float:
+        return abs(xr-x)
+    elif type(xr)==list or type(xr)==np.ndarray:
+        abs_diff=np.abs(np.array(xr)-np.array(x))
+        return np.max(abs_diff)
     """Funkcja obliczająca normę L-nieskończoność. Powinna działać zarówno na 
     wartościach skalarnych, listach, jak i wektorach biblioteki numpy.
 
@@ -71,4 +107,4 @@ def L_inf(
         (float): Wartość normy L-nieskończoność.
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+
